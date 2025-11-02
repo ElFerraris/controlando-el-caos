361a3be81d19cbf90835df5aead5d70a4f8fdb34
ElFerraris <galol.exe@alumnes.ilerna.com>
2025-11-02 01:46:47 +0100

    Leer números por Teclado
    Añadido a la Calculadora para que lea los números por Teclado.

--- src/ControladorDelCaos/Calculadora.java
+++ src/ControladorDelCaos/Calculadora.java
@@ -1,5 +1,5 @@
 package ControladorDelCaos;
-
+import java.util.Scanner;
 public class Calculadora {
 
 	public Calculadora() {
@@ -10,14 +10,19 @@ public class Calculadora {
 		// TODO Auto-generated method stub
 
 		System.out.println("=== Calculadora Básica ===");
-        int a = 10;
-        int b = 5;
+        Scanner sc = new Scanner(System.in);
+		int a = 0, b = 0;
  
-        System.out.println("Suma: " + (a + b));
+		System.out.print("\rValor A: ");
+		a = sc.nextInt();
+		
+		System.out.print("Valor B: ");
+		b = sc.nextInt();
+		
+        System.out.println("\rSuma: " + (a + b));
         System.out.println("Resta: " + (a - b));
         System.out.println("Multiplicación: " + (a * b));
         System.out.println("División: " + (a / b));
 
-		
 	}
 }
